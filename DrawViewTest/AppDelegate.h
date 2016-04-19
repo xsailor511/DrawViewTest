e71e7a00fef2ff23a5a1d27f27a7ed44c951b965
richard <xsailor511@163.com>
2016-04-19 09:44:24 +0800

    Initial Commit

---
+++ DrawViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawViewTest
+//
+//  Created by richard on 16/4/19.
+//  Copyright © 2016年 ztesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
